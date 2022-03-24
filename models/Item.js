0ec079aea67aa0c037b285df29f35ac8534a6057
Firat Göktepe <firat@naxnordic.com>
2022-03-24 17:16:37 +0300

    some fixes about models

--- models/Item.js
+++ models/Item.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const ItemSchema = mongoose.Schema({
+const ItemSchema = new Schema({
     title: {
         type: String,
         required: true
--- models/User.js
+++ models/User.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const { isEmail } = require('validator');
 
-const UserSchema = mongoose.Schema({
+const UserSchema = new Schema({
     name: {
         type: String,
         required: true
