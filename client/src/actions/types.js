889451abffe4e44ad0aeb02a101d4c072be8b6f0
Firat Göktepe <firat@naxnordic.com>
2022-03-29 16:31:02 +0300

    added action types

---
+++ client/src/actions/types.js
@@ -0,0 +1,33 @@
+// write action types for items, cart, user, orders, loading ,errors
+
+// Items
+export const GET_ITEMS = 'GET_ITEMS';
+export const ADD_ITEM = 'ADD_ITEM';
+export const DELETE_ITEM = 'DELETE_ITEM';
+export const ITEMS_LOADING = 'ITEMS_LOADING';
+export const UPDATE_ITEM = 'UPDATE_ITEM';
+
+// Cart
+export const ADD_TO_CART = 'ADD_TO_CART';
+export const DELETE_FROM_CART = 'DELETE_FROM_CART';
+export const CART_LOADING = 'CART_LOADING';
+export const GET_CART = 'GET_CART';
+
+// Orders
+export const GET_ORDERS = 'GET_ORDERS';
+export const ORDERS_LOADING = 'ORDERS_LOADING';
+export const CHECKOUT = 'CHECKOUT';
+
+// User
+export const USER_LOADING = 'USER_LOADING';
+export const USER_LOADED = 'USER_LOADED';
+export const AUTH_ERROR = 'AUTH_ERROR';
+export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
+export const LOGIN_FAIL = 'LOGIN_FAIL';
+export const LOGOUT_SUCCESS = 'LOGOUT_SUCCESS';
+export const REGISTER_SUCCESS = 'REGISTER_SUCCESS';
+export const REGISTER_FAIL = 'REGISTER_FAIL';
+
+// Errors
+export const GET_ERRORS = 'GET_ERRORS';
+export const CLEAR_ERRORS = 'CLEAR_ERRORS';
