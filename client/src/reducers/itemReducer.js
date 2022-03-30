70f136c6e61f039eab92d966e80fbac11d12cac5
Firat Göktepe <firat@naxnordic.com>
2022-03-30 16:50:28 +0300

    reducer for items is ready

---
+++ client/src/reducers/itemReducer.js
@@ -0,0 +1,50 @@
+import { 
+    ADD_ITEM,
+    DELETE_ITEM,
+    UPDATE_ITEM,
+    GET_ITEMS,
+    ITEMS_LOADING
+} from '../actions/itemActions'
+
+const initialState = {
+    items: [],
+    loading: false 
+}
+
+export default function( state = initialState, action) {
+    switch(action.type) {
+        case GET_ITEMS:
+            return {
+                ...state,
+                items: action.payload,
+                loading: false
+            }
+        case ADD_ITEM:
+            return {
+                ...state,
+                items: [action.payload, ...state.items]
+            }
+        case DELETE_ITEM:
+            return {
+                ...state,
+                items: state.items.filter(item => item._id !== action.payload)
+            }
+        case UPDATE_ITEM:
+            const { id, data } = action.payload
+            return {
+                ...state,
+                items: state.items.map(item => {
+                    if(item._id === id) {
+                        item = data
+                    } 
+                })
+            }
+        case ITEMS_LOADING:
+            return {
+                ...state,
+                loading: true
+            }
+        default:
+            return state
+    }
+}
