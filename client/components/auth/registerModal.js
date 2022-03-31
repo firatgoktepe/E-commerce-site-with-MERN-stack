f0f5f77219de109e7a11f18eac15f23547c6ebea
Firat Göktepe <firat@naxnordic.com>
2022-03-31 16:30:48 +0300

    added register component and connected it into redux

---
+++ client/components/auth/registerModal.js
@@ -0,0 +1,122 @@
+import { Component } from 'react'
+import { 
+   
+    Modal,
+    ModalHeader,
+    ModalBody,
+    Button,
+    Form,
+    FormGroup,
+    Label,
+    Input,
+    NavLink,
+    Alert
+
+ } from 'reactstrap'
+import { connect } from 'react-redux'
+import { register } from '../../actions/authActions'
+import PropTypes from 'prop-types'
+import { clearErrors } from '../../actions/errorActions'
+
+class RegisterModal extends Component {
+    state = {
+        modal: false,
+        name: '',
+        email: '',
+        password: '',
+        msg: null
+    }
+
+    static propTypes = {
+        isAuthenticated: PropTypes.bool,
+        error: PropTypes.object.isRequired,
+        register: PropTypes.func.isRequired,
+        clearErrors: PropTypes.func.isRequired
+    }
+
+    componentDidUpdate(prevProps) {
+        const { error, isAuthenticated } = this.props
+        if(error !== prevProps.error) {
+            // check for register error
+            if(error.id === 'REGISTER_FAIL') {
+                this.setState({ msg: error.msg.msg })
+            } else {
+                this.setState({ msg: null })
+            }
+        }
+        // if authenticated, close modal
+        if(this.state.modal) {
+            if(isAuthenticated) {
+                this.toggle()
+            }
+        }
+    }
+
+    toggle = () => {
+        // clear errors
+        this.props.clearErrors()
+        this.setState({
+            modal: !this.state.modal
+        })
+    }
+
+    onChange = (e) => {
+        this.setState({ [e.target.name]: e.target.value })
+    }
+
+    onSubmit = (e) => {
+        e.preventDefault()
+
+        const { name, email, password } = this.state
+
+        // create user object
+        const newUser = {
+            name,
+            email,
+            password
+        }
+
+        // attempt to register
+        this.props.register(newUser)
+    }
+
+    render() {
+        return (
+            <div className="container">
+                <Button color="info" className="btn btn-sm">
+                    <NavLink onClick={this.toggle} href="#"><span className="text-dark"><b>Register</b></span></NavLink>
+                </Button>
+                <Modal isOpen={this.state.modal} toggle={this.toggle}>
+                    <ModalHeader toggle={this.toggle}>Register</ModalHeader>
+                    <ModalBody>
+                        { this.state.msg ? <Alert color="danger">{this.state.msg}</Alert> : null }
+                        <Form onSubmit={this.onSubmit}>
+                            <FormGroup>
+                                <Label for="name">Name</Label>
+                                <Input type="text" name="name" id="name" placeholder="Name" className="mb-3" onChange={this.onChange} />
+                            
+                                <Label for="email">Email</Label>
+                                <Input type="email" name="email" id="email" placeholder="Email" className="mb-3" onChange={this.onChange} />
+                            
+                                <Label for="password">Password</Label>
+                                <Input type="password" name="password" id="password" placeholder="Password" className="mb-3" onChange={this.onChange} />
+                        
+                                <Button color="dark" style={{ marginTop: '2rem' }} block>Register</Button>
+                            </FormGroup>
+                        </Form>
+                    </ModalBody>
+                </Modal>
+            </div>
+        )
+    }
+}
+
+const mapStateToProps = (state) => {
+    return {
+        isAuthenticated: state.auth.isAuthenticated,
+        error: state.error
+    }
+}
+
+export default connect(mapStateToProps, { register, clearErrors })(RegisterModal)
+
