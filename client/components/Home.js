23a2f0556f08c4da4ac9ec07d21362c5010afdd7
Firat Göktepe <firat@naxnordic.com>
2022-04-04 16:25:47 +0300

    home page done

--- client/components/Home.js
+++ client/components/Home.js
@@ -18,4 +18,52 @@ class Home extends Component {
         addToCart: PropTypes.func.isRequired,
         user: PropTypes.object.isRequired
     }
+
+    onAddToCart = async (id, productId) => {
+        await this.props.addToCart(id, productId, 1)
+        alert('Item added to Cart')
+    }
+
+    render() {
+        const { items } = this.props.item
+        const user = this.props.user
+        return (
+            <div>
+                <AppNavbar />
+                <Container>
+                    <div className="row">
+                        { items.map(item => (
+                          <div className="col-md-4">
+                            <Card key={item.id} className="mb-4">
+                                <CardBody>
+                                    <CardTitle tag="h5">{item.title}</CardTitle>
+                                    <CardSubtitle tag="h6">{item.proce}</CardSubtitle>
+                                    <CardText>{item.category}</CardText>
+                                    { this.props.isAuthenticated ? 
+
+                                    <Button
+                                        color="success"
+                                        size="sm"
+                                        onClick={this.onAddToCart.bind(this, user._id, item._id)}
+                                    >Add to Cart</Button> : null 
+
+                                    }
+                                
+                                </CardBody>
+                            </Card>
+                          </div>  
+                        )) }
+                    </div>
+                </Container>
+            </div>
+        )
+    }
 }
+
+const mapStateToProps = state => ({
+    item: state.item,
+    isAuthenticated: state.auth.isAuthenticated,
+    user: state.auth.user
+})
+
+export default connect(mapStateToProps, { getItems, addToCart })(Home)
