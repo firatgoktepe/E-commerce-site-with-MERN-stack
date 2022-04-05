668d89ac08e7c5e27ae9f0d2dd84d128fbd0b63d
Firat Göktepe <firat@naxnordic.com>
2022-04-05 13:31:00 +0300

    stripe checkout component added

---
+++ client/components/Checkout.js
@@ -0,0 +1,16 @@
+import StripeCheckout from 'react-stripe-checkout';
+
+const STRIPE_PUBLISHABLE = 'pk_test_********************';
+
+const onToken = (user,checkout) => token => 
+    checkout(user, token.id);
+
+const Checkout = ({ amount, user, checkout }) => 
+    <StripeCheckout
+      amount={amount*100}
+      token={onToken(user,checkout)}
+      currency='INR'
+      stripeKey={STRIPE_PUBLISHABLE}
+/>
+
+export default Checkout;
